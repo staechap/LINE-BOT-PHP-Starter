dd38c91c9c29874fd1f335aa0f640f39fac36263
staechap <mon.oracle@gmail.com>
2017-12-14 19:58:54 +0700

    fixed IP

--- bot.php
+++ bot.php
@@ -1,4 +1,8 @@
 <?php
+
+$proxy = 'proxyurl:port';
+$proxyauth = 'username:password';
+
 $access_token = 'fU1OP4og1nbjBKb2vCt4itTIGEJK3x72QEn5+1jZugsR2bAmZpo+amvq7z77OJa7R5UvMygtwDlbZuZTIagBAp4SgorifcM4QBAgejZD2IMORH0IJz9hQ3ElIqQRVuX9Tez3mxIhdAJoDi1qCjqNygdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
@@ -32,6 +36,9 @@ if (!is_null($events['events'])) {
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
 			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+			curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
--- verify.php
+++ verify.php
@@ -1,11 +1,16 @@
 <?php
-$access_token = 'fU1OP4og1nbjBKb2vCt4itTIGEJK3x72QEn5+1jZugsR2bAmZpo+amvq7z77OJa7R5UvMygtwDlbZuZTIagBAp4SgorifcM4QBAgejZD2IMORH0IJz9hQ3ElIqQRVuX9Tez3mxIhdAJoDi1qCjqNygdB04t89/1O/w1cDnyilFU=';
 
+$proxy = 'proxyurl:port';
+$proxyauth = 'username:password';
+$access_token = 'fU1OP4og1nbjBKb2vCt4itTIGEJK3x72QEn5+1jZugsR2bAmZpo+amvq7z77OJa7R5UvMygtwDlbZuZTIagBAp4SgorifcM4QBAgejZD2IMORH0IJz9hQ3ElIqQRVuX9Tez3mxIhdAJoDi1qCjqNygdB04t89/1O/w1cDnyilFU=';
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
 $ch = curl_init($url);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
